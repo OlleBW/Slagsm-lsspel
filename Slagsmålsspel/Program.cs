e93e4ec37ea96cea302da59ec99fcbdf935f5ff4
Olle Brink Wennberg <olle.brinkwennberg@elev.ga.ntig.se>
2021-10-12 11:01:33 +0200

    sdvbjisdosd

--- Slagsmålsspel/Program.cs
+++ Slagsmålsspel/Program.cs
@@ -2,9 +2,18 @@
 
 Random generator = new Random();
 
-int hp = (100);
+int hp1 = (100);
 int hp2 = (100);
-int damage = generator.Next(0, 25);
+ToolBox.WriteCentered("Welcome to my fighting game! Plese write your name to start.");
+string name = Console.ReadLine();
+ToolBox.WriteCentered($"Hello {name}. You are fighting against player2.");
+ToolBox.WriteCentered("Press Enter to punch player2.");
+Console.ReadLine();
+int damage = generator.Next(10, 40);
+hp2 = hp2 - damage;
+Console.WriteLine($"You punched player2 and did {damage} damage. player2 now has {hp2} hp left.");
+
+
 
 
 Console.ReadLine();
